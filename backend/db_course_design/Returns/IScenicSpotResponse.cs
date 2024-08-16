553e845b6a16bd2af052b5795bd31e90ee9b5407
linchang77 <2250420@tongji.edu.cn>
2024-08-16 15:58:24 +0800

    完成景点api

---
+++ backend/db_course_design/Returns/IScenicSpotResponse.cs
@@ -0,0 +1,23 @@
+﻿using EntityFramework.Models;
+
+namespace db_course_design.Returns
+{
+    public class ScenicSpotResponse
+    {
+        public string? ScenicSpotName { get; set; }
+
+        public decimal ScenicSpotId { get; set; }
+
+        public string? CityName { get; set; }
+
+        public string? ScenicSpotGrade { get; set; }
+
+        public string? ScenicSpotIntroduction { get; set; }
+
+        public string? ScenicSpotLocation { get; set; }
+
+        public virtual City? CityNameNavigation { get; set; }
+
+        public virtual ICollection<ScenicSpotTicket> ScenicSpotTickets { get; set; } = new List<ScenicSpotTicket>();
+    }
+}
