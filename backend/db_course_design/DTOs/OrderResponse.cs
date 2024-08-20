d3b323b0db292ebe344ff8c07f3eabb6443ab333
2252942@tongji.edu.cn <2252942@tongji.edu.cn>
2024-08-20 23:23:36 +0800

    增加被忽略的文件

---
+++ backend/db_course_design/DTOs/OrderResponse.cs
@@ -0,0 +1,62 @@
+﻿namespace db_course_design.DTOs
+{
+    /*--封装订单返回属性的类--*/
+    public class OrderResponse
+    {
+        public int OrderId { get; set; }
+        public string OrderType { get; set; }
+        public string Status { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public decimal? Price { get; set; }
+    }
+    public class GuideOrderDetail : OrderResponse
+    {
+        public DateTime? ServiceBeginDate { get; set; }
+        public DateTime? ServiceEndDate { get; set; }
+        public string Service { get; set; }
+        public byte GuideId { get; set; }
+        public string GuideName { get; set; }
+        public string GuideGender { get; set; }
+    }
+    public class HotelOrderDetail : OrderResponse
+    {
+        public string? CityName { get; set; }
+        public string? HotelName { get; set; }
+        public string? HotelLocation { get; set; }
+        public DateTime? CheckInDate { get; set; }
+        public DateTime? CheckOutDate { get; set; }
+        public string RoomType { get; set; }
+    }
+    public class ScenicOrderDetail : OrderResponse
+    {
+        public string? CityName { get; set; }
+        public string ScenicSpotName { get; set; }
+        public string TicketType { get; set; }
+        public decimal? TicketNumber { get; set; }
+        public DateTime TicketDate { get; set; }
+    }
+    public class TourOrderDetail : OrderResponse
+    {
+        public byte? GroupId { get; set; }
+        public string? GroupName { get; set; }  // 建议名字起旅行目的地
+        public byte? GuideId { get; set; }
+        public string GuideName { get; set; }
+        public string GuideGender { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+    public class VehicleOrderDetail : OrderResponse
+    {
+        public string? VehicleId { get; set; }
+        public string? VehicleType { get; set; }
+        public decimal TicketId { get; set; }
+        public string? TicketType { get; set; }
+        public string TicketUserName { get; set; } = null!;
+        public DateTime? TicketDepartureTime { get; set; }
+        public DateTime? TicketArrivalTime { get; set; }
+        public string? TicketDepartureCity { get; set; }
+        public string? TicketArrivalCity { get; set; }
+        public string? TicketDepartureStation { get; set; }
+        public string? TicketArrivalStation { get; set; }
+    }
+}
---
+++ backend/db_course_design/Profiles/VehicleProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using db_course_design.DTOs;
+using db_course_design.Services.impl;
+using EntityFramework.Models;
+
+namespace db_course_design.Profiles
+{
+    public class VehicleProfile : Profile
+    {
+        public VehicleProfile()
+        {
+            CreateMap<VehicleSchedule, VehicleResponse>()
+                .ForMember(dest => dest.DepartureTime, opt => opt.Ignore())
+                .ForMember(dest => dest.ArrivalTime, opt => opt.Ignore())
+                .ForMember(dest => dest.DepartureCity, opt => opt.Ignore())
+                .ForMember(dest => dest.ArrivalCity, opt => opt.Ignore())
+                .ForMember(dest => dest.DepartureStation, opt => opt.Ignore())
+                .ForMember(dest => dest.ArrivalStation, opt => opt.Ignore());
+            CreateMap<VehicleTicket, VehicleResponse>()
+                .ForMember(dest => dest.DepartureTime, opt => opt.MapFrom(src => src.TicketDepartureTime))
+                .ForMember(dest => dest.ArrivalTime, opt => opt.MapFrom(src => src.TicketArrivalTime))
+                .ForMember(dest => dest.DepartureCity, opt => opt.MapFrom(src => src.TicketDepartureCity))
+                .ForMember(dest => dest.ArrivalCity, opt => opt.MapFrom(src => src.TicketArrivalCity))
+                .ForMember(dest => dest.DepartureStation, opt => opt.MapFrom(src => src.TicketDepartureStation))
+                .ForMember(dest => dest.ArrivalStation, opt => opt.MapFrom(src => src.TicketArrivalStation));
+            CreateMap<VehicleScheduleRequest, VehicleSchedule>();
+            CreateMap<VehicleTicketRequest, VehicleTicket>();
+        }
+    }
+}
