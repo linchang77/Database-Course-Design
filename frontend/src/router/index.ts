939a4516ea2bbf70f462e608c94a43f9b8216963
Dai <2253092@tongji.edu.cn>
2024-08-23 23:52:00 +0800

    完成酒店功能框架

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -334,6 +334,33 @@ export const dynamicRoutes: RouteRecordRaw[] = [
       }
     ]
   },
+  {
+    path: "/hotel",
+    component: Layouts,
+    meta: {
+      hidden: true
+    },
+    children: [
+      {
+        path: "detail",
+        component: () => import("@/views/hotel/detail.vue"),
+        name: "Detail",
+        meta: {
+          title: "酒店详情",
+          hidden: true
+        }
+      },
+      {
+        path: "order",
+        component: () => import("@/views/hotel/order.vue"),
+        name: "Order",
+        meta: {
+          title: "酒店订单",
+          hidden: true
+        }
+      },
+    ]
+  }
 
   // {
   //   path: "/permission",
