8a471f72aeb4d946a699ef7413076bfe060624d4
Dai <2253092@tongji.edu.cn>
2024-08-27 22:47:23 +0800

    暂时commit

---
+++ frontend/src/views/hotels-management/index.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { ref } from "vue"
+
+defineOptions({
+  name: "Hotels-management"
+})
+
+const text = ref("")
+</script>
+
+<template>
+  <div class="app-container">
+    <el-card header="酒店管理">
+    </el-card>
+  </div>
+</template>
