b2c186e93a794e04e36f76878e2809479cefef7d
oknik <1254583588@qq.com>
2024-08-27 14:07:48 +0800

    添加导航栏图标

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -133,6 +133,45 @@ export const dynamicRoutes: RouteRecordRaw[] = [
     ]
   },
   {
+    path: "/transport",
+    component: Layouts,
+    redirect: "/transport/airplane",
+    name: "Transport",
+    meta: {
+      title: "出行",
+      svgIcon: "transport",
+      roles: ["visitor","admin"]
+    },
+    children: [
+      {
+        path: "airplane",
+        component: () => import("@/views/transport/airplane/index.vue"),
+        name: "Airplane",
+        meta: {
+          title: "飞机"
+        }
+      },
+      {
+        path: "bus",
+        component: () => import("@/views/transport/bus/index.vue"),
+        name: "Bus",
+        meta: {
+          title: "大巴",
+          keepAlive: true
+        }
+      },
+      {
+        path: "train",
+        component: () => import("@/views/transport/train/index.vue"),
+        name: "Train",
+        meta: {
+          title: "火车",
+          keepAlive: true
+        }
+      }
+    ]
+  },
+  {
     path: "/",
     component: Layouts,
     meta:{
@@ -208,22 +247,6 @@ export const dynamicRoutes: RouteRecordRaw[] = [
   {
     path: "/",
     component: Layouts,
-    children: [
-      {
-        path: "self-center",
-        component: () => import("@/views/self-center/index.vue"),
-        name: "Self-center",
-        meta: {
-          title: "个人中心",
-          svgIcon: "self-center",
-          affix: true
-        }
-      }
-    ]
-  },
-  {
-    path: "/",
-    component: Layouts,
     meta:{
       roles: ["visitor","guide"]
     },
@@ -235,6 +258,22 @@ export const dynamicRoutes: RouteRecordRaw[] = [
         meta: {
           title: "我的订单",
           svgIcon: "my-orders",
+          affix: true
+        }
+      }
+    ]
+  },
+  {
+    path: "/",
+    component: Layouts,
+    children: [
+      {
+        path: "self-center",
+        component: () => import("@/views/self-center/index.vue"),
+        name: "Self-center",
+        meta: {
+          title: "个人中心",
+          svgIcon: "self-center",
           affix: true
         }
       }
@@ -274,45 +313,6 @@ export const dynamicRoutes: RouteRecordRaw[] = [
           title: "业绩",
           svgIcon: "performance",
           affix: true
-        }
-      }
-    ]
-  },
-  {
-    path: "/transport",
-    component: Layouts,
-    redirect: "/transport/airplane",
-    name: "Transport",
-    meta: {
-      title: "出行",
-      svgIcon: "transport",
-      roles: ["visitor","admin"]
-    },
-    children: [
-      {
-        path: "airplane",
-        component: () => import("@/views/transport/airplane/index.vue"),
-        name: "Airplane",
-        meta: {
-          title: "飞机"
-        }
-      },
-      {
-        path: "bus",
-        component: () => import("@/views/transport/bus/index.vue"),
-        name: "Bus",
-        meta: {
-          title: "大巴",
-          keepAlive: true
-        }
-      },
-      {
-        path: "train",
-        component: () => import("@/views/transport/train/index.vue"),
-        name: "Train",
-        meta: {
-          title: "火车",
-          keepAlive: true
         }
       }
     ]
