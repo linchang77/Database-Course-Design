661bf8b574e59218cee380dc03e843f495490045
linchang77 <2250420@tongji.edu.cn>
2024-08-28 23:43:33 +0800

    添加推荐功能，并且修改了CORS

--- backend/db_course_design/Services/IScenicSpotService.cs
+++ backend/db_course_design/Services/IScenicSpotService.cs
@@ -1,6 +1,7 @@
 ﻿using db_course_design.DTOs;
 using db_course_design.Services.impl;
 using db_course_design.Common;
+using Microsoft.AspNetCore.Mvc;
 /*--景点管理的服务抽象类，简化控制器逻辑--*/
 namespace db_course_design.Services
 {
@@ -21,5 +22,6 @@ namespace db_course_design.Services
         Task<AdultChildTicketResponse?> GetTodayTicketInfoAsync(string scenicSpotName);
         Task<AdultChildTicketResponse?> GetTicketInfoByDateAsync(string scenicSpotName, DateTime date);
         Task<bool> PurchaseTicketAsync(string scenicSpotName, string type, DateTime date, CreateScenicSpotOrderRequest orderRequest, int number=1);
+        Task<IEnumerable<ScenicSpotResponse>> GetRecommendedScenicSpotsAsync();
     }
 }
