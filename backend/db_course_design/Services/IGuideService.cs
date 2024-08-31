2e664bcd2951ae9e04947360f2dc54159824b420
linchang77 <2250420@tongji.edu.cn>
2024-08-31 15:20:17 +0800

    增加了导游的增删改

--- backend/db_course_design/Services/IGuideService.cs
+++ backend/db_course_design/Services/IGuideService.cs
@@ -1,4 +1,5 @@
 ﻿using db_course_design.DTOs;
+using db_course_design.Services.impl;
 
 namespace db_course_design.Services
 {
@@ -10,5 +11,9 @@ namespace db_course_design.Services
         Task<List<GuideResponse>> GetGuideByPersonAsync(byte? GuideId, string? name);
         // 根据价格区间、级别筛选
         Task<List<GuideResponse>> GetGuideByAbilityAsync(decimal? minCost, decimal? maxCost, string? grade);
+        Task<GuideResponse> GetGuideByIdAsync(byte GuideId);
+        Task<GuideResponse> AddGuideAsync(GuideRequest guideRequest);
+        Task<GuideResponse> UpdateGuideAsync(byte GuideId, GuideRequest guideRequest);
+        Task<bool> DeleteGuideAsync(byte GuideId);
     }
 }
--- backend/db_course_design/Services/impl/GuideService.cs
+++ backend/db_course_design/Services/impl/GuideService.cs
@@ -6,14 +6,39 @@ using Microsoft.AspNetCore.Mvc.ModelBinding;
 using AutoMapper;
 using db_course_design.Profiles;
 using db_course_design.Common;
+using static db_course_design.Controler.AuthController;
 
 namespace db_course_design.Services.impl
 {
+    public class GuideRequest
+    {
+        public string? GuideName { get; set; }
+
+        public string? GuideGender { get; set; }
+
+        public string? GuideIntroduction { get; set; }
+
+        public decimal? GuideSalary { get; set; }
+
+        public string? GuidePerformanceLevel { get; set; }
+
+        public string? GuideSeniority { get; set; }
+
+        public decimal? GuidePrice { get; set; }
+
+        public string? ProfilePicture { get; set; }
+
+        public bool? GuideFree { get; set; }
+
+        public string? Password { get; set; }
+    }
+
     public class GuideService : IGuideService
     {
         private readonly ModelContext _context;
 
         public IMapper _mapper { get; }
+
 
         public GuideService(ModelContext context)
         {
@@ -47,10 +72,74 @@ namespace db_course_design.Services.impl
                 .Select(o => _mapper.Map<GuideResponse>(o)).ToListAsync();
             return guides;
         }
+
+        public async Task<GuideResponse> GetGuideByIdAsync(byte GuideId)
+        {
+            var guide = await _context.Guides.FindAsync(GuideId);
+
+            if (guide == null)
+            {
+                return null;
+            }
+
+            return _mapper.Map<GuideResponse>(guide);
+        }
+
         /*--预约导游--*
         public async Task<bool> GuideReservationAsync(GuideReservationResponse response)
         {
             var Id = response.GuideId
         }*/
+
+        /*--添加导游信息--*/
+        public async Task<GuideResponse> AddGuideAsync(GuideRequest guideRequest)
+        {
+            guideRequest.Password= SaltedPassword.HashPassword(guideRequest.Password, SaltedPassword.salt);
+            var guide = _mapper.Map<Guide>(guideRequest);
+
+            _context.Guides.Add(guide);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<GuideResponse>(guide);
+        }
+
+
+        /*--修改导游信息--*/
+        public async Task<GuideResponse> UpdateGuideAsync(byte GuideId, GuideRequest guideRequest)
+        {
+            guideRequest.Password = SaltedPassword.HashPassword(guideRequest.Password, SaltedPassword.salt);
+            var guide = await _context.Guides.FindAsync(GuideId);
+
+            if (guide == null)
+            {
+                return null;
+            }
+
+            _mapper.Map(guideRequest, guide);
+
+            _context.Guides.Update(guide);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<GuideResponse>(guide);
+        }
+
+
+
+        /*--删除导游信息--*/
+        public async Task<bool> DeleteGuideAsync(byte GuideId)
+        {
+            var guide = await _context.Guides.FindAsync(GuideId);
+
+            if (guide == null)
+            {
+                return false;
+            }
+
+            _context.Guides.Remove(guide);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
     }
 }
