e67e858aa25ff76608cc6e3467ff3c056cb30b1b
linchang77 <2250420@tongji.edu.cn>
2024-09-02 22:00:17 +0800

    完成用户管理的api

--- backend/db_course_design/Profiles/ProfileProfile.cs
+++ backend/db_course_design/Profiles/ProfileProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using EntityFramework.Models;
 using db_course_design.DTOs;
+using db_course_design.Services.impl;
 
 namespace db_course_design.Profiles
 {
@@ -10,6 +11,9 @@ namespace db_course_design.Profiles
         {
             CreateMap<User, UserProfileResponse>()
                 .ForMember(dest => dest.PhoneNumbers, opt => opt.MapFrom(src => src.UserPhoneNumbers.Select(x => x.PhoneNumber).ToList()));
+
+            CreateMap<UserRequest, User>();
+            CreateMap<UserRequest, UserProfileResponse>();
             CreateMap<Guide, GuideProfileResponse>()
                 .ForMember(dest => dest.PhoneNumbers, opt => opt.MapFrom(src => src.GuidePhoneNumbers.Select(x => x.PhoneNumber).ToList()))
                 .ForMember(dest => dest.Regions, opt => opt.MapFrom(src => src.GuideRegions.Select(x => x.GuidedRegion).ToList()));
